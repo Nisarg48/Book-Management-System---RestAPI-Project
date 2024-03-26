1e536c15b55a7177e4467cf5c0199082940593b8
Shivpatel2694 <shivpatel2694@gmail.com>
2024-03-26 20:47:48 +0530

    reviewed methods and added multirole security

--- BookRestAPIProject/src/main/java/BookRestAPI/Dao/BookTransactionRecordDao.java
+++
@@ -1,7 +0,0 @@
-package BookRestAPI.Dao;
-
-public interface BookTransactionRecordDao {
-    public void BorrowBook(int isbn, int id);
-
-    public void ReturnBook(int isbn, int id);
-}
--- BookRestAPIProject/src/main/java/BookRestAPI/Service/BookTransactionRecordService.java
+++
@@ -1,7 +0,0 @@
-package BookRestAPI.Service;
-
-public interface BookTransactionRecordService {
-    public void BorrowBook(int isbn, int id);
-
-    public void ReturnBook(int isbn, int id);
-}
